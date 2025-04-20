ec5d30babe2da38fa6a947bccc25d3f2f983ae44
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-20 17:43:15 -0300

    alterando os textos da pagina home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent],
+  imports: [FooterComponent, NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
