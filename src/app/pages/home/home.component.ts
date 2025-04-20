a026927d99d3d62c48e6bb6977fcc52956198a69
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-20 16:28:42 -0300

    finalizando a pagina home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
