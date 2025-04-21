4cd7cd477038dffd9964f0652d0876caf4b57bba
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-21 20:35:53 -0300

    criando a pagina de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    },
+    {
+        path: "cadastro",
+        component: SignupComponent
     }
 ];
