37c51d068ee1ee0e2a9d31a3c3723a829684f6c9
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-21 21:59:55 -0300

    adicionando animações na homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
-
+import {
+  Component,
+  QueryList,
+  ViewChildren,
+  ElementRef,
+  AfterViewInit
+} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 @Component({
   selector: 'app-home',
   imports: [FooterComponent, NavBarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      state('out', style({ opacity: 0, transform: 'translateY(30px)' })),
+      state('in', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition('out => in', animate('1000ms ease-out'))
+    ])
+  ]
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChildren('targetElement') targetElements!: QueryList<ElementRef>;
+  visibleElements: boolean[] = [];
 
+  ngAfterViewInit() {
+    this.visibleElements = new Array(this.targetElements.length).fill(false);
+
+    this.targetElements.forEach((element, index) => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.visibleElements[index] = true;
+            observer.unobserve(entry.target);
+          }
+        });
+      });
+
+      observer.observe(element.nativeElement);
+    });
+  }
 }
