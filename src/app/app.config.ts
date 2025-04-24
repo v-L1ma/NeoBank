939c05ccd3ea131a2d70084bf211ef85ba542c42
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-24 19:30:04 -0300

    criando formulario de login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+
+  ngOnInit():void {
+
+  this.loginForm = new FormGroup({
+    email : new FormControl<String>("", [Validators.required, Validators.minLength(5)]),
+    password : new FormControl<String>("",[Validators.required, Validators.minLength(5)])
+  });
+  }
+
+  get email(){
+    return this.loginForm.get('email')!
+  }
+
+  get password(){
+    return this.loginForm.get('password')!
+  }
+
+  submit(){
+    console.log(this.loginForm.value)
+  }
 
 }
