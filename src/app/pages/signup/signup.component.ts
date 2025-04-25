49fe3cfa3579e3db9f9032bec1f0b513dce941a7
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-24 21:37:52 -0300

    criando o formulario de cadastro

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterModule],
+  imports: [ReactiveFormsModule,RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+  signupForm!: FormGroup;
+
+  ngOnInit():void{
+    this.signupForm = new FormGroup({
+      name: new FormControl("", Validators.required),
+      email: new FormControl("", Validators.required),
+      password: new FormControl("", Validators.required)
+    });
+  }
+
+  get name(){
+    return this.signupForm.get('name')
+  }
+
+  get email(){
+    return this.signupForm.get('email')
+  }
+
+  get password(){
+    return this.signupForm.get('password')
+  }
+
+  submit(){
+    console.log(this.signupForm.value)
+  }
 
 }
